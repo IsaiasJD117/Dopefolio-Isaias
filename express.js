d3dd18c471884b64c88dbbac58f68b70415097f4
IsaiasJD117 <104098161+IsaiasJD117@users.noreply.github.com>
2024-02-27 14:58:15 -0500

    first commit

---
+++ express.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+const xss = require('xss');
+require('dotenv').config();
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/send', async (req, res) => {
+    let { name, email, message } = xss(req.body);
+    
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+      },
+      
+    });
+    
+    let mailOptions = {
+        from: email,
+        to: 'isaiasthewebdeveloper@gmail.com',
+        subject: 'New Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.send('Email sent successfully');
+    } catch (error) {
+        console.log(error);
+        res.send('Failed to send email');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
